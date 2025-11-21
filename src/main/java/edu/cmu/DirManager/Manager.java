496ff70519c956f85e2b3b9aa4cb1ae8178dd30c
jessicajiang <786213911@qq.com>
2025-11-21 00:02:24 -0500

    Finish Lab12

--- src/main/java/edu/cmu/DirManager/Manager.java
+++ src/main/java/edu/cmu/DirManager/Manager.java
@@ -1,17 +1,16 @@
 package edu.cmu.DirManager;
 
 public class Manager {
-    private DirOps dirOps;
-    
     /**
      * Creates a new directory at the specified path.
      *
      * @param path the path where the new directory should be created
+     * @param dirOps dirOps the directory operations implementation to use
      * @return 0 if the directory creation was successful
      *        -1 if the directory already exists,
      *        -2 if the path is invalid
      */
-    public int newDirectory(String path) {
+    public int newDirectory(String path, DirOps dirOps) {
         if (dirOps.checkDirectoryExists(path)) {
             return -1;
         } else if (!dirOps.checkPathValid(path)) {
